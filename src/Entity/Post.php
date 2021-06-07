e2d8fa43359b71d1353d6fb575a1c9a751be52ec
Lorenzo Panizza <lorenzoapdesign@hotmail.com>
2021-06-07 11:06:51 +0200

    Create table Post

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -17,8 +17,25 @@ class Post
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $message;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(?string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
     }
 }

6a0ea21f1867addd60594815aa064b068ac7b952
Lorenzo Panizza <lorenzoapdesign@hotmail.com>
2021-06-07 13:18:38 +0200

    Add some faked users into the database

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,26 +2,42 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
+
+
+use App\Entity\Post;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use App\Entity\Post;
 use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+    private function encode($user, $plaintextpassword)
+    {
+        return $this->passwordEncoder->encodePassword(
+            $user,
+            $plaintextpassword
+        );
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create();
         for ($i = 0; $i < 10; $i++) {
-            // $simpleUser = new User();
-            // $simpleUser->setNickname($faker->firstName());
-            // $simpleUser->setPassword($this->encode($simpleUser, "toto"));
-            // $simpleUser->setRoles(['ROLE_USER']);
-            // $manager->persist($simpleUser);
+            $simpleUser = new User();
+            $simpleUser->setUsername($faker->firstName());
+            $simpleUser->setPassword($this->encode($simpleUser, "toto"));
+            $simpleUser->setRoles(['ROLE_USER']);
+            $manager->persist($simpleUser);
             for ($j = 0; $i < random_int(0, 10); $j++) {
                 $onePost = new Post();
-                $onePost->setMessage($faker->text(60));
-                // $onePost->setPseudo($faker->lastName());
+                $onePost->setMessage($faker->text(255));
                 $manager->persist($onePost);
             }
         }
